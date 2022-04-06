7d154f835c74341a9d4b6fcd8d8f4b98f87662c4
João Vitor Esposte Campos <102496892+JoaoVitorEsposte@users.noreply.github.com>
2022-04-05 22:54:47 -0300

    Áudios incluídos e Configurados via JS
    Áudios de cada nota foram incluídos e configurados via JS para serem executados ao clicar nos seus respectivos botões. Tive dificuldades em alguns ids e nomes de arquivos, mas percebi os caracteres que não podiam ser utilizados e os substituí.
    
    Neste ponto a ideia central do projeto está finalizada, porém irei continuar fazendo melhorias no código.

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const listaNotas = document.querySelectorAll('button');
+const listaSons = document.querySelectorAll('audio');
 
+function tocaSom (idSom) {
+	document.querySelector(idSom).play();
+}
+
+//Adiciona os estilos CSS aos botões do HTML Automaticamente
 for(let i = 0; i < listaNotas.length; i++){
 	if(listaNotas[i].textContent != "Corda Solta"){
 		listaNotas[i].classList.add('nota');
@@ -7,4 +13,22 @@ for(let i = 0; i < listaNotas.length; i++){
 		listaNotas[i].classList.add('nota-solta');
 	}
 
+
+}
+
+//Associa os sons aos botões do site, e faz a distinção entre cordas soltas e notas dentro das cordas
+for(let i = 0; i < listaNotas.length; i++){
+	const botaoNota = listaNotas[i];
+	const idAudio = `#nota-${botaoNota.value}`; //Template String
+
+	if(botaoNota.textContent != "Corda Solta"){
+		botaoNota.onclick = function () {
+				tocaSom(idAudio);
+			}
+	}else if(botaoNota.textContent === "Corda Solta"){
+		botaoNota.onclick = function () {
+			tocaSom(`#${botaoNota.value}`);
+		}
+	}
+	
 }
